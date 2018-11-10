05ce2f33bc29f01d9c6e502eef38a2720999cd8e
Sean Kelly <home@seank.com>
2018-11-10 09:21:16 -0800

    Refactor routes and views

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -5,23 +5,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', user: req.user });
-});
-
-router.get('/register', function(req, res) {
-  res.render('register', { });
-});
-
-router.post('/register', function(req, res) {
-  Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
-      if (err) {
-          return res.render('register', { account : account });
-      }
-
-      passport.authenticate('local')(req, res, function () {
-          res.redirect('/');
-      });
-  });
+  if (req.user) {
+    res.render('dashboard', {title: 'Express', user: req.user});
+  } else {
+    res.redirect('/login');
+  }
 });
 
 router.get('/login', function(req, res) {
--- web/src/routes/users.js
+++ web/src/routes/users.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+router.get('/add', function(req, res) {
+  res.render('users/add', { });
+});
+
+router.post('/add', function(req, res) {
+  Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+      if (err) {
+          return res.render('users/add', { account : account });
+      }
+
+      passport.authenticate('local')(req, res, function () {
+        res.redirect('/');
+      });
+  });
+});
+
 module.exports = router;
